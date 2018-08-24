c014372e987ae7048a6185bdad54b01caa955dd5
Happyfrozenfire <42660416+Happyfrozenfire@users.noreply.github.com>
2018-08-23 20:20:21 -0400

    Add files via upload

---
+++ Source-Code/Main.java
@@ -0,0 +1,11 @@
+import com.modloader.*;
+import javax.swing.SwingUtilities;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    UserInterface ui = new UserInterface();
+    SwingUtilities.invokeLater(ui);
+  }
+}
